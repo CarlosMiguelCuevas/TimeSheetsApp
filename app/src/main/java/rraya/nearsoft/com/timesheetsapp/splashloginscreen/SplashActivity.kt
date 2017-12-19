13f4e67b81eff47ff50dc01841bab757f4d5c12f
Omar Aguilar <oaguilar@nearsoft.com>
2017-12-19 11:29:29 -0700

    Adding fragment to activity

--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/splashloginscreen/SplashActivity.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/splashloginscreen/SplashActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import rraya.nearsoft.com.timesheetsapp.R
 
+
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val frag = SplashView()
+        supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
     }
+
 }
