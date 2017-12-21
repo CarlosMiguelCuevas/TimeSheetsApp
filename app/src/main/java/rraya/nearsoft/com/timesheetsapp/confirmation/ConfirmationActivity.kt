c49b647b7412291d94ddca9fc1b5f0d0003944d0
Omar Aguilar <oaguilar@nearsoft.com>
2017-12-21 12:04:34 -0700

    User submitted on time.

--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/confirmation/ConfirmationActivity.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/confirmation/ConfirmationActivity.kt
@@ -2,6 +2,7 @@ package rraya.nearsoft.com.timesheetsapp.confirmation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_confirmation.*
@@ -14,8 +15,13 @@ import javax.inject.Inject
 
 class ConfirmationActivity : AppCompatActivity() {
 
-    private var userDidGood: Boolean = false
+    private var submittedOnTime: Boolean = false
     @Inject lateinit var gifRepo: IGifsRepository
+
+    companion object {
+        const val TAG = "ConfirmationActivity"
+        const val DID_SUBMIT_ON_TIME = "ConfirmationActivity"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,23 +30,33 @@ class ConfirmationActivity : AppCompatActivity() {
 
         (application as TimeSheetsApp).getAppComponent()?.inject(this)
 
-        userDidGood = intent.getBooleanExtra("DID_GOOD", false)
+        submittedOnTime = savedInstanceState?.getBoolean(DID_SUBMIT_ON_TIME) ?: intent.getBooleanExtra(DID_SUBMIT_ON_TIME, false)
 
-        if(userDidGood){
+        if(submittedOnTime){
+            thank.text = getText(R.string.thank_you)
+            doing_great.text = getText(R.string.doing_great)
             gifRepo.wellDoneGif.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe({
                 val gifDrawable = GifDrawable(it)
                 gifView.setImageDrawable(gifDrawable)
             }, {
+                Log.e(TAG, "Error loading gif", it)
                 //TODO: sorry not sorry, add the image or something default
             })
         }else{
+            thank.text = getText(R.string.thank_you_but)
+            doing_great.text = getText(R.string.next_time)
             gifRepo.notGoodGif.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe({
                 val gifDrawable = GifDrawable(it)
                 gifView.setImageDrawable(gifDrawable)
             }, {
+                Log.e(TAG, "Error loading gif", it)
                 //TODO: sorry not sorry, add the image or something default
             })
         }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putBoolean(DID_SUBMIT_ON_TIME, submittedOnTime)
     }
 }
--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/data/GifsRepository.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/data/GifsRepository.kt
@@ -1,15 +1,12 @@
 package rraya.nearsoft.com.timesheetsapp.data
 
-import android.os.Build
 import io.reactivex.Single
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import rraya.nearsoft.com.timesheetsapp.BuildConfig
 import rraya.nearsoft.com.timesheetsapp.network.GiphyApi
 import rraya.nearsoft.com.timesheetsapp.network.GiphyResponse
-import java.io.ByteArrayOutputStream
 import java.util.*
-import javax.inject.Inject
 
 
 class GifsRepository(val api: GiphyApi) : IGifsRepository {
@@ -17,6 +14,14 @@ class GifsRepository(val api: GiphyApi) : IGifsRepository {
 
     override fun getWellDoneGif(): Single<ByteArray> {
         return api.getGiphyGoodGifs().map {
+            getRandomGifFromResponse(it)
+        }.map {
+            getImageFromUrl(it)
+        }
+    }
+
+    override fun getNotGoodGif(): Single<ByteArray> {
+        return api.getGiphyNotGoodGifs().map {
             getRandomGifFromResponse(it)
         }.map {
             getImageFromUrl(it)
@@ -43,25 +48,6 @@ class GifsRepository(val api: GiphyApi) : IGifsRepository {
 
         val request = Request.Builder().url(imageUrl).build()
         val response = httpClientBuilder.build().newCall(request).execute()
-        val inputStream = response.body()?.byteStream()
-        val buffer = ByteArrayOutputStream()
-        val data = ByteArray(1024)
-        var byte1: Int
-        inputStream?.let {
-            do{
-                byte1 = inputStream.read(data, 0, data.size)
-                buffer.write(data, 0, byte1)
-            }while(byte1 != -1)
-        }
-        buffer.flush()
-        return buffer.toByteArray()
-    }
-
-    override fun getNotGoodGif(): Single<ByteArray> {
-        return api.getGiphyNotGoodGifs().map {
-            getRandomGifFromResponse(it)
-        }.map {
-            getImageFromUrl(it)
-        }
+        return response.body()!!.bytes()
     }
 }
