3ea6573d4ad34d8420b56bcd9e99facf05adf158
Mathreyu <rraya@nearsoft.com>
2018-01-05 15:24:28 -0700

    Notifications:
    - Added style to notification (modifications pending: Add functionality to send, add proper size to message)

--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/notifications/AlarmReceiver.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/notifications/AlarmReceiver.kt
@@ -1,10 +1,10 @@
 package rraya.nearsoft.com.timesheetsapp.notifications
 
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.support.v4.app.NotificationCompat
 import rraya.nearsoft.com.timesheetsapp.R
 import rraya.nearsoft.com.timesheetsapp.timesheetform.TimeSheetActivity
 
@@ -12,39 +12,34 @@ import rraya.nearsoft.com.timesheetsapp.timesheetform.TimeSheetActivity
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        //Get notification manager to manage/send notifications
         val notificationHelper = NotificationHelper()
-
-
-        //Intent to invoke app when click on notification.
-        //In this sample, we want to start/launch this sample app when user clicks on notification
         val intentToRepeat = Intent(context, TimeSheetActivity::class.java)
-        //set flag to restart/relaunch the app
+
         intentToRepeat.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
-        //Pending intent to handle launch of Activity in intent above
         val pendingIntent = PendingIntent.getActivity(context, notificationHelper.ALARM_TYPE_RTC, intentToRepeat, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        //Build notification
         val repeatedNotification = buildLocalNotification(context, pendingIntent).build()
-
-        //Send local notification
         notificationHelper.getNotificationManager(context).notify(notificationHelper.ALARM_TYPE_RTC, repeatedNotification)
     }
 
-    fun buildLocalNotification(context: Context, pendingIntent: PendingIntent): NotificationCompat.Builder {
+    fun buildLocalNotification(context: Context, pendingIntent: PendingIntent): Notification.Builder {
 
         //TODO Add action to send timesheet as is.
         val sendAsIsIntent = null
-        var sendAsIsAction = NotificationCompat.Action.Builder(R.drawable.send_timesheet_icon, "Send", sendAsIsIntent).build()
-        var editAction = NotificationCompat.Action.Builder(R.drawable.icon_edit_grey, "Edit", pendingIntent).build()
+        val sendAsIsAction = Notification.Action(R.drawable.send_timesheet_icon, "Send", sendAsIsIntent)
+        val editAction = Notification.Action(R.drawable.edit_timesheet, "Edit", pendingIntent)
 
-        return NotificationCompat.Builder(context)
+        var clientName = "Cliente"
+        var hours = "40 hrs"
+        return Notification.Builder(context)
                 .setContentIntent(pendingIntent)
-                .setContentTitle("Nearsoft timesheets")
+                .setContentTitle(clientName)
+                .setContentText(hours)
                 .setSmallIcon(R.drawable.timesheet_notif_icon)
-                .addAction(sendAsIsAction)
+                .setColor(context.resources.getColor(R.color.colorPrimary))
                 .addAction(editAction)
-                .setAutoCancel(true) as NotificationCompat.Builder
+                .addAction(sendAsIsAction)
+                .setAutoCancel(true) as Notification.Builder
     }
 }
--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/notifications/NotificationHelper.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/notifications/NotificationHelper.kt
@@ -16,27 +16,15 @@ class NotificationHelper {
 
 
     fun scheduleRepeatingRTCNotification(context: Context) {
-        //get calendar instance to be able to select what time notification should be scheduled
         val calendar = Calendar.getInstance()
         calendar.setTimeInMillis(System.currentTimeMillis())
-        //Setting time of the day (8am here) when notification will be sent every day (default)
-        calendar.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY)
-//        calendar.set(Calendar.HOUR_OF_DAY, 10)
-//        calendar.set(Calendar.MINUTE, 38)
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY)
+        calendar.set(Calendar.HOUR_OF_DAY, 12)
 
-        //Setting intent to class where Alarm broadcast message will be handled
         val intent = Intent(context, AlarmReceiver::class.java)
-        //Setting alarm pending intent
         alarmIntentRTC = PendingIntent.getBroadcast(context, ALARM_TYPE_RTC, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        //getting instance of AlarmManager service
         alarmManagerRTC = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        //Setting alarm to wake up device every day for clock time.
-        //AlarmManager.RTC_WAKEUP is responsible to wake up device for sure, which may not be good practice all the time.
-        // Use this when you know what you're doing.
-        //Use RTC when you don't need to wake up device, but want to deliver the notification whenever device is woke-up
-        //We'll be using RTC.WAKEUP for demo purpose only
         alarmManagerRTC!!.setInexactRepeating(AlarmManager.RTC_WAKEUP,
                 calendar.timeInMillis, AlarmManager.INTERVAL_DAY, alarmIntentRTC)
     }
