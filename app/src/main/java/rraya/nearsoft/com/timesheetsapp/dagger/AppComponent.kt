191a896f5fe59442196020590afaeade8356f819
Carlos Miguel Cuevas Ochoa <ccuevas@nearsoft.com>
2018-03-21 00:32:47 -0700

    add example of how to initialize the subcomponet's module when this needs parameters

--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/dagger/AppComponent.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/dagger/AppComponent.kt
@@ -1,6 +1,8 @@
 package rraya.nearsoft.com.timesheetsapp.dagger
 
 import dagger.Component
+import rraya.nearsoft.com.timesheetsapp.splashloginscreen.dagger.SplashModule
+import rraya.nearsoft.com.timesheetsapp.splashloginscreen.dagger.TimesheetModule
 import javax.inject.Singleton
 
 @Singleton
@@ -12,8 +14,10 @@ import javax.inject.Singleton
 ])
 interface AppComponent {
 
-    fun SplashSubComponent(): SplashComponent
-    fun TimeSheetFormhSubComponent(): TimeSheetFormComponent
+    //Educativo, se pone asi si el modulo del subcoempoennte requiere parametro
+    fun SplashSubComponent(module: SplashModule): SplashComponent
+    //Edicativo, se puede escribir asi o sin el parametro, si es que el modulo tiene constructor default
+    fun TimeSheetFormhSubComponent(module: TimesheetModule): TimeSheetFormComponent
     fun ConfirmationSubComponent(): ConfirmationComponent
     fun NotificationSubComponent(): NotificationComponent
 
--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/splashloginscreen/dagger/SplashModule.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/splashloginscreen/dagger/SplashModule.kt
@@ -6,13 +6,21 @@ import rraya.nearsoft.com.timesheetsapp.dagger.scopes.ActivityViewScope
 import rraya.nearsoft.com.timesheetsapp.data.IDataRepository
 import rraya.nearsoft.com.timesheetsapp.splashloginscreen.SplashPresenter
 import rraya.nearsoft.com.timesheetsapp.splashloginscreen.SplashViewPresenterContract
+import javax.inject.Named
 
 @Module
-class SplashModule {
+class SplashModule(var messageToShow: String) {
 
     @ActivityViewScope
     @Provides
     internal fun provideSplashPresenter(dataRepository: IDataRepository): SplashViewPresenterContract.Presenter {
         return SplashPresenter(dataRepository)
+    }
+
+    @ActivityViewScope
+    @Named("SplashMessage")
+    @Provides
+    internal fun provideMessage(): String {
+        return messageToShow
     }
 }
