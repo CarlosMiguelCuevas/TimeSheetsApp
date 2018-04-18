b17f66a8c380636e8498961888f3a52995223984
Carlos Miguel Cuevas Ochoa <ccuevas@nearsoft.com>
2018-04-17 21:01:44 -0700

    upgrade kotlin plugin version

--- app/src/main/java/rraya/nearsoft/com/timesheetsapp/timesheetnotification/helpers/AlarmManagerHelper.kt
+++ app/src/main/java/rraya/nearsoft/com/timesheetsapp/timesheetnotification/helpers/AlarmManagerHelper.kt
@@ -3,6 +3,7 @@ package rraya.nearsoft.com.timesheetsapp.timesheetnotification.helpers
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
+import rraya.nearsoft.com.timesheetsapp.common.extensions.setDefaultTimeReminder
 import rraya.nearsoft.com.timesheetsapp.timesheetnotification.receivers.AlarmReceiver
 import java.util.*
 
@@ -11,17 +12,18 @@ class AlarmManagerHelper {
     fun scheduleTimesheetReminder(context: Context, clientName: String) {
 
         var alarmManager: AlarmManager? = null
-        var alarmReceiverPedingIntent: PendingIntent? = null
+        var alarmReceiverPendingIntent: PendingIntent? = null
         val calendar = Calendar.getInstance()
-//        calendar.setDefaultTimeReminder()
+//        TODO: comment ot this to see notification immediately
+        calendar.setDefaultTimeReminder()
 
-        alarmReceiverPedingIntent = AlarmReceiver.getPendingIntentWith(context, clientName)
+        alarmReceiverPendingIntent = AlarmReceiver.getPendingIntentWith(context, clientName)
         alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         //I used setRepeateing instead of inexact because according to documentation `With setInexactRepeating(), you can't specify a custom interval the way you can with setRepeating()`
         //https://developer.android.com/training/scheduling/alarms.html
         alarmManager!!.setRepeating(AlarmManager.RTC_WAKEUP,
-                calendar.timeInMillis, AlarmManager.INTERVAL_DAY * 7, alarmReceiverPedingIntent)
+                calendar.timeInMillis, AlarmManager.INTERVAL_DAY * 7, alarmReceiverPendingIntent)
     }
 
 }
